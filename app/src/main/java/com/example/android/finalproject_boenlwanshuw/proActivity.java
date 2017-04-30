5acbdd69358251d93271806fa8908aa7ab04153a
Wanshu Wang <ww2446@tc.columbia.edu>
2017-04-30 15:00:13 -0400

    login

--- app/src/main/java/com/example/android/finalproject_boenlwanshuw/proActivity.java
+++ app/src/main/java/com/example/android/finalproject_boenlwanshuw/proActivity.java
@@ -1,13 +1,189 @@
 package com.example.android.finalproject_boenlwanshuw;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class proActivity extends AppCompatActivity {
+    public class SecondActivity extends AppCompatActivity {
+        private FirebaseAuth mAuth;
+        private FirebaseAuth.AuthStateListener mAuthListener;
+        private String TAG = "SecondActivity";
+        private FirebaseDatabase database = FirebaseDatabase.getInstance();
+        private DatabaseReference myRef;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pro);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_pro);
+
+            mAuth = FirebaseAuth.getInstance();
+            mAuthListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    if (user != null) {
+                        // User is signed in
+                        Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                        myRef = database.getReference(user.getUid());
+                        displayInfo();
+                    } else {
+                        // User is signed out
+                        Log.d(TAG, "onAuthStateChanged:signed_out");
+
+                        Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }
+            };
+
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            mAuth.addAuthStateListener(mAuthListener);
+        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+            if (mAuthListener != null) {
+                mAuth.removeAuthStateListener(mAuthListener);
+            }
+        }
+
+        public void displayInfo(){
+            TextView nameF = (TextView)findViewById(R.id.showName);
+            TextView ageF = (TextView)findViewById(R.id.showAge);
+            TextView genderF = (TextView)findViewById(R.id.showGender);
+
+            updateField(nameF,"name");
+            updateField(ageF,"age");
+            updateField(genderF,"gender");
+        }
+
+
+        public void updateField(final TextView filed, String key){
+            myRef.child(key).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    String value = dataSnapshot.getValue(String.class);
+
+                    if (value != null) {
+                        filed.setText(value);
+                    } else {
+                        filed.setText("Not Set");
+                    }
+
+                    Log.d(TAG, "Value is: " + value);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Failed to read value
+                    Log.w(TAG, "Failed to read value.", error.toException());
+                }
+            });
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle item selection
+            switch (item.getItemId()) {
+                case R.id.logout:
+                    mAuth.signOut();
+                    return true;
+                case R.id.editProfile:
+                    startActivity(new Intent(SecondActivity.this, editActivity.class));
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
     }
+    //    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_second);
+//        mDatabase = FirebaseDatabase.getInstance();
+//
+//        mAuth = FirebaseAuth.getInstance();
+//        mAuthListener = new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                FirebaseUser user = firebaseAuth.getCurrentUser();
+//                if (user != null) {
+//
+//                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+//
+//                    userRef = mDatabase.getReference(user.getUid());
+//
+//                    final TextView nameField = (TextView) findViewById(R.id.showName);
+//                    userRef.child("name").addValueEventListener(new ValueEventListener()
+//
+//                    {
+//                        @Override
+//                        public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//                            String value = dataSnapshot.getValue(String.class);
+//                            Log.d(TAG, "Value is: " + value);
+//                            if (value == null) nameField.setText("Not set");
+//                            else nameField.setText(value);
+//
+//                        }
+//
+//                        @Override
+//                        public void onCancelled(DatabaseError databaseError) {
+//
+//                        }
+//                    });
+//
+//                } else {
+//
+//                    Log.d(TAG, "onAuthStateChanged:signed_out");
+//                    startActivity(new Intent(SecondActivity.this, MainActivity.class));
+//                    finish();
+//                }
+//            }
+//        };
+//    }
+//
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        mAuth.addAuthStateListener(mAuthListener);
+//    }
+//
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        if (mAuthListener != null) {
+//            mAuth.removeAuthStateListener(mAuthListener);
+//        }
+//    }
+
 }

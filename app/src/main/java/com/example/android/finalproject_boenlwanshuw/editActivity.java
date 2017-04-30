74a17a1fe6bc81c9d119dfaecce9a77741024e0f
Wanshu Wang <ww2446@tc.columbia.edu>
2017-04-30 14:28:49 -0400

    layout

---
+++ app/src/main/java/com/example/android/finalproject_boenlwanshuw/editActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.finalproject_boenlwanshuw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
